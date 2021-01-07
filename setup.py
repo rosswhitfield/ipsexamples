750dc25f3cfa4c9de4603c728a2176565e180d3b
Ross Whitfield <whitfieldre@ornl.gov>
2021-01-07 11:41:06 -0500

    Change IPS install from pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="ipsexamples",
     version="0.1.0",
     install_requires=[
-        'ipsframework==0.2.0'
+        'ipsframework==0.2.1'
     ],
     packages=find_packages(),
     scripts=['scripts/integrator.py',
